7419a641524de3e9ca4f2c07f823fadaa4c0849b
Mike Goldin <mike.goldin@consensys.net>
2015-06-03 22:28:04 +0000

    Comments.

--- buildheth.sh
+++ buildheth.sh
@@ -4,12 +4,16 @@ echo --YOU WILL NEED TO ENTER SOME INFORMATION BEFORE LETTING THIS RUN!--
 sleep 2
 echo --SO HOLD ON JUST A MINUTE BEFORE WALKING AWAY--
 sleep 5 
+
+# Initialize Ubuntu
 sudo apt-get update
 sudo apt-get -y upgrade
 
+# Get some important tools
 sudo apt-get -y install git
 sudo apt-get -y install gpgv2
 
+# Install git-repo
 mkdir bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
@@ -17,11 +21,13 @@ chmod a+x ~/bin/repo
 mkdir hask
 cd hask
 
+# Setup git-repo and pull the modules.
 repo init -u http://github.com/jamshidh/ethereumH
 echo --OKAY, NOW YOU CAN GO GET A COFFEE WHILE THIS RUNS.--
 sleep 5
 repo sync
 
+# Install ghc and cabal
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
@@ -32,11 +38,13 @@ sudo apt-get install -y zlib1g-dev
 cabal update
 cabal install alex happy
 
+# These libraries are required.
 sudo apt-get -y install libpq-dev
 sudo apt-get -y install libleveldb-dev
 
 ./build
 
+# Update PATH
 echo "export PATH=~/bin:~/.cabal/bin:/opt/ghc/7.8.4/bin:$PATH" >> ~/.bashrc
 source .bashrc
  
