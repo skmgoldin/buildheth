86693e5cd5bd2118b60471e0806faee0ac318730
Mike Goldin <mike.goldin@consensys.net>
2015-06-03 22:07:29 +0000

    Initial commit.

---
+++ buildhask.sh
@@ -0,0 +1,38 @@
+export PATH=~/bin:~/.cabal/bin:/opt/ghc/7.8.4/bin:$PATH
+
+echo --YOU WILL NEED TO ENTER SOME INFORMATION BEFORE LETTING THIS RUN!--
+sleep 2
+echo --SO HOLD ON JUST A MINUTE BEFORE WALKING AWAY--
+sleep 5 
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install git
+sudo apt-get -y install gpgv2
+
+mkdir bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+mkdir hask
+cd hask
+
+repo init -u http://github.com/jamshidh/ethereumH
+echo --OKAY, NOW YOU CAN GO GET A COFFEE WHILE THIS RUNS.--
+sleep 5
+repo sync
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:hvr/ghc
+sudo apt-get update
+sudo apt-get install -y cabal-install-1.20 ghc-7.8.4
+sudo apt-get install -y zlib1g-dev
+/opt/cabal/1.20/bin/cabal update
+/opt/cabal/1.20/bin/cabal install cabal-install
+cabal update
+cabal install alex happy
+
+sudo apt-get -y install libpq-dev
+sudo apt-get -y install libleveldb-dev
+
+./build
